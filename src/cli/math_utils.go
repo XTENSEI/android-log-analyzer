a02b84c57786b8f7018a00713abcd423f41903ee
XTENSEI <andriegalutera@gmail.com>
2026-03-21 19:21:11 +0800

    feat(go): add math utilities

---
+++ src/cli/math_utils.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Math constants:")
+	fmt.Println("Pi:", math.Pi)
+	fmt.Println("E:", math.E)
+	fmt.Println("Sqrt2:", math.Sqrt2)
+	fmt.Println("MaxInt:", math.MaxInt)
+}
