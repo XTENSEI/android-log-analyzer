0cb7203a3f00eaacb050ad18fbb18b772452a032
XTENSEI <andriegalutera@gmail.com>
2026-03-21 20:20:02 +0800

    feat(api): add parameter defaults decorator

---
+++ src/api/param_defaults.py
@@ -0,0 +1,9 @@
+def params(**kwargs):
+    def decorator(func):
+        def wrapper(*args, **opts):
+            for k, v in kwargs.items():
+                if k not in opts:
+                    opts[k] = v
+            return func(*args, **opts)
+        return wrapper
+    return decorator
