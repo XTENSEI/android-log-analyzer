5085b6fb4fc132037ec8a3b4917f994ddd52f61c
XTENSEI <andriegalutera@gmail.com>
2026-03-21 19:28:32 +0800

    feat(go): add stack data structure

---
+++ src/cli/stack.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Stack struct {
+	items []interface{}
+}
+
+func (s *Stack) Push(item interface{}) {
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() interface{} {
+	if len(s.items) == 0 {
+		return nil
+	}
+	item := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return item
+}
+
+func (s *Stack) Peek() interface{} {
+	if len(s.items) == 0 {
+		return nil
+	}
+	return s.items[len(s.items)-1]
+}
+
+func main() {
+	s := Stack{}
+	s.Push(1)
+	s.Push("hello")
+	fmt.Println("Pop:", s.Pop())
+	fmt.Println("Peek:", s.Peek())
+}
