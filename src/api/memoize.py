18de7be76f3cffd294d047549c89a0e1debaac29
XTENSEI <andriegalutera@gmail.com>
2026-03-21 20:01:01 +0800

    feat(api): add memoization decorator

---
+++ src/api/memoize.py
@@ -0,0 +1,11 @@
+def memoize(func):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapper
+
+@memoize
+def expensive_function(n):
+    return n * 2
