27fe49ef1a907409b2bc9375490f2f1d0cbbcdd3
XTENSEI <andriegalutera@gmail.com>
2026-03-22 13:25:26 +0800

    feat(sepolicy): add wrap utilities

---
+++ src/sepolicy/wrap.py
@@ -0,0 +1,17 @@
+"""
+Wrap Module
+
+Wrapper functions.
+"""
+
+from typing import Callable, Any
+
+
+def wrap(func: Callable) -> Callable:
+    """Wrap a function."""
+    return func
+
+
+def unwrap(func: Callable) -> Callable:
+    """Unwrap a function."""
+    return func
